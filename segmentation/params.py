1ac2222407dcb609bc61d7b69e53801eec3fd98d
Anders Schreiber <schreiber-dk@hotmail.com>
2025-06-12 11:39:36 +0200

    setup segmentation folders and setup segmentation api

---
+++ segmentation/params.py
@@ -0,0 +1,7 @@
+import os
+import numpy as np
+
+LOCAL_REGISTRY_PATH =  os.path.join(os.path.expanduser('~'),
+                                    "code",
+                                    "schreiberdk",
+                                    "lewagon-breast-cancer-project")
