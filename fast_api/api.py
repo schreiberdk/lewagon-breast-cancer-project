f6faf0d26689b6226462f283e7f54759df3cc4eb
Luciano Quaglia Casal <lucianoqcasal@gmail.com>
2025-06-18 11:25:36 +0200

    Fix(FastAPI): Newest model and api pushed and  deployed to GCP

--- fast_api/api.py
+++ fast_api/api.py
@@ -13,35 +13,28 @@ import matplotlib.pyplot as plt
 
 app = FastAPI()
 
+
 @app.get("/")
 def index():
     return {"status": "Up and running."}
 
 
+app.state.preprocessor = Preprocessor()
 
 # --------------------------- CLASSIFICATION -----------------------------------
 app.state.modelclass = load_model(
-    "models/EfficientNetB0_mammography_model_16_6_2025.keras", compile=False)
-app.state.preprocessor = Preprocessor()
+    "models/EfficientNetB0_mammography_model_17_6_2025.keras", compile=False)
 
 @app.post('/classification')
 async def receive_image(img: UploadFile=File(...)):
-    ### Receiving and decoding the image
+    # Receiving and decoding the image
     contents = await img.read()
-    print(f'Image type after img.read: {type(contents)}')
-
-    # try exept?
     contents = Image.open(io.BytesIO(contents))
-    print(f'Image type after opening with io.Bytes: {type(contents)}')
     contents = img_to_array(contents)
-    print(f'Image type after img_to_array conversion: {type(contents)}')
-    print(f'Image shape after img_to_array conversion: {contents.shape}')
 
-    # PREPROCESS image for prediction
+    # Preprocess image for prediction
     preprocessor = app.state.preprocessor
     contents = preprocessor.preprocess_image(contents)
-    print(f'Image type after preprocessing: {type(contents)}')
-    print(f'Image shape after preprocessing: {contents.shape}')
     contents = contents.reshape((-1, 224, 224, 3))
 
     # Prediction
@@ -49,13 +42,11 @@ async def receive_image(img: UploadFile=File(...)):
     res = str(model.predict(contents)[-1][-1])
     return {"probability":res}
 
-    # return  {"Probability of Malignant Breast Cancer": f"{round(prob,2)}%"}
-
-
 
 # --------------------------- SEGMENTATION ------------------------------------
 app.state.modelseg = load_model("models/unet_seg_model_11_6_2025_2.keras",
                                 compile=False)
+
 def plot_overlay(image, mask, alpha=0.5, mask_color='Reds'):
     """
     image: numpy array, shape (H, W, 3)
@@ -78,19 +69,18 @@ def plot_overlay(image, mask, alpha=0.5, mask_color='Reds'):
 
 @app.post('/segmentation')
 async def receive_image(img: UploadFile=File(...)):
-    ### Receiving and decoding the image
+    # Receiving and decoding the image
     contents = await img.read()
     pil_image = Image.open(io.BytesIO(contents)).convert("RGB")
     pil_image = pil_image.resize((224, 224))
     if app.state.preprocessor.is_black_on_white(img_to_array(pil_image)):
                 pil_image = ImageOps.invert(pil_image)
-    #import ipdb; ipdb.set_trace()
-    # Step 2: Preprocess image
+
+    # Preprocess image
     img_array = img_to_array(pil_image) / 255.0  # shape: (224, 224, 3)
     img_for_model = img_array.reshape((-1, 224, 224, 3))
 
     # Prediction
-    #import ipdb; ipdb.set_trace()
     model = app.state.modelseg
     res = model.predict(img_for_model)
     res_binary = (res > 0.5).astype(np.uint8).squeeze()
